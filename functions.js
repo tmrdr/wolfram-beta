02dafd11bedf53d098f89d52e0b50a1e9781a596
Tobias Murphy <murphy.tobias@gmail.com>
2017-01-26 15:38:24 -0800

    merge function + factorialize

--- functions.js
+++ functions.js
@@ -54,4 +54,28 @@ return answer;
 };
 
 
-// 
+// merge and sort two arrays seperated by comma
+function merge(a1, a2) {
+  var i1 = 0;
+  var i2 = 0;
+  var results = [];
+
+  while (i1 < a1.length && i2 < a2.length) {
+    if (a1[i1] < a2[i2]) {
+      results.push(a1[i1]);
+      i1++;
+    } else {
+      results.push(a2[i2]);
+      i2++;
+    }
+  }
+  while (i1 < a1.length) {
+    results.push(a1[i1]);
+    i1++;
+  }
+  while (i2 < a2.length) {
+    results.push(a2[i2]);
+    i2++;
+  }
+  return results;
+}
