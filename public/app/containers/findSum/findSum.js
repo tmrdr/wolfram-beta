5ebd8c05096190569e337b02daafb8e5c9123cc1
Tommy-Lim <tlim24@gmail.com>
2017-01-26 16:10:38 -0800

    added findSum

--- public/app/containers/findSum/findSum.js
+++ public/app/containers/findSum/findSum.js
@@ -9,6 +9,34 @@ function FindSumCompCtrl(){
 
   var findSumComp = this;
   findSumComp.phrase = "finding the sums";
+  findSumComp.input = " 1 4 5 6 7 8 ";
+
+  findSumComp.calculate = function(){
+    console.log("hello");
+    var array = findSumComp.input.split(" ").map(function(item){
+      return parseInt(item);
+    })
+    var newArray = [];
+    array.forEach(function(item){
+      if(isNaN(item)){
+        //do nothing
+      } else{
+        newArray.push(item);
+      }
+    })
+
+    function addAll(array){
+      var sum = null;
+      for (i = 0; i < array.length; i++){
+        sum = sum + array[i];
+        }
+      return sum;
+    };
+
+
+    findSumComp.sum = addAll(newArray);
+  }
+
 }
 
 
