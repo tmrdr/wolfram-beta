248bf157d14d778798e577dbde323457adcd88be
Tommy-Lim <tlim24@gmail.com>
2017-01-27 12:25:41 -0800

    added regex to service

--- public/app/services/data-services.js
+++ public/app/services/data-services.js
@@ -4,7 +4,11 @@ angular.module('App')
 function DataServices(){
 
   this.stringToArray = function(str){
+    str = str.replace(/\[/g,"");
+    str = str.replace(/\]/g,"");
+    str = str.replace(/\,/g,"");
 
+    console.log(str)
     var array = str.split(" ").map(function(item){
       return parseInt(item);
     })
